4956949ad03d92543469b94e94f96216af330a1a
TeemuRaittinen <113996657+TeemuRaittinen@users.noreply.github.com>
2024-04-13 21:41:09 +0300

    Login hahmottelua, ei mitään lopullista.

--- index.js
+++ index.js
@@ -3,34 +3,27 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 
 const app = express();
-
+app.set('view engine','handlebars');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.engine('handlebars',exphbs.engine({
+    defaultLayout: 'main'}
+));
+
 app.get('/', (req,res) => {
     //res.send("Testing");
-    //res.render('index');
     res.render('index',
-{   title: 'Home',
-    author: 'Teemu'
-})
+);
 });
-
-app.get('/contact', (req,res) => {
+app.get('/register', (req,res) => {
     //res.send("Testing");
-    res.render('contact');
+    res.render('register');
 });
-
-app.get('/list', (req,res) => {
+app.get('/dashboard', (req,res) => {
     //res.send("Testing");
-    res.render('list',
-    
-    {
-        title: 'Game Scores',
-        author: 'Teemu',
-    }
-)
+    res.render('dashboard');
 });
 //folder for static files like styles/jpg's and so on.
 app.use(express.static('public'));
