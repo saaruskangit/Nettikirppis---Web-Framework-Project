910ea888eaa7da630cef468fafc72f4e76b02807
saaraSaara <saara.rinne@student.hamk.fi>
2024-04-29 00:31:51 +0300

    Validaatiota lisää. Tuotteiden näyttäminen kortteina taulukon sijaan.

--- index.js
+++ index.js
@@ -103,6 +103,8 @@ app.get("/", (req, res) => {
       check("hinta", "Syötä 'Tuotteen hinta' - kenttään ainoastaan numeroita.")
         .exists()
         .isNumeric(),
+      check("kuvaus", "Syötä tuotteelle kuvaus.").exists().notEmpty(),
+      check("kuva", "Lisää ilmoitukseen kuva.").notEmpty(),
     ],
     async (req, res) => {
       const errors = validationResult(req);
@@ -235,38 +237,36 @@ app.get("/varaus", (req, res) => {
 // Varaus schema
 const Varaus = require("./models/varaus");
 
-  // CREATE toiminto varauksen tekemiselle
-  app.post(
-    "/varaus",
-    urlencodedParser,
-    [
-      check("nimi", "Anna tuotteelle nimi, syötä vähintään 3 kirjainta.")
-        .exists()
-        .isLength({ min: 3 }),
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        const alert = errors.array();
-        // Send the response with validation errors
-        return res.render("varaus", { alert });
-      }
-  
-      // Create a new Varaus instance with request body
-      const uusiVaraus = new Varaus(req.body);
-      try {
-        // Save the Varaus instance to the database
-        await uusiVaraus.save();
-        // Send the response with success message
-        return res.render("varaus", { alert2: "Varaus lisätty onnistuneesti" });
-      } catch (err) {
-        // Send the response with error message
-        return res.status(400).json({ status: "Failed", message: err });
-      }
+// CREATE toiminto varauksen tekemiselle
+app.post(
+  "/varaus",
+  urlencodedParser,
+  [
+    check("nimi", "Anna tuotteelle nimi, syötä vähintään 3 kirjainta.")
+      .exists()
+      .isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const alert = errors.array();
+      // Send the response with validation errors
+      return res.render("varaus", { alert });
     }
-  );
-  
 
+    // Create a new Varaus instance with request body
+    const uusiVaraus = new Varaus(req.body);
+    try {
+      // Save the Varaus instance to the database
+      await uusiVaraus.save();
+      // Send the response with success message
+      return res.render("varaus", { alert2: "Varaus lisätty onnistuneesti" });
+    } catch (err) {
+      // Send the response with error message
+      return res.status(400).json({ status: "Failed", message: err });
+    }
+  }
+);
 
 //Lisää Tuote peruskäyttäjille
 app.get("/userlisaatuote", (req, res) => {
@@ -307,6 +307,8 @@ app.post(
     check("hinta", "Syötä 'Tuotteen hinta' - kenttään ainoastaan numeroita.")
       .exists()
       .isNumeric(),
+    check("kuvaus", "Syötä tuotteelle kuvaus.").exists().notEmpty(),
+    check("kuva", "Lisää ilmoitukseen kuva.").isEmpty(),
   ],
 
   async (req, res) => {
@@ -339,10 +341,10 @@ app.post(
 // Varauksien tarkastelu
 app.get("/varaukset", async (req, res) => {
   try {
-      const varaus = await Varaus.find({ alkuPaivays: req.query.selectedDate });
+    const varaus = await Varaus.find({ alkuPaivays: req.query.selectedDate });
 
-      res.json({ varaus });
+    res.json({ varaus });
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 });
